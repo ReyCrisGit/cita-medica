c39a03ebcc469a06eeaf54c9d3d96b5f9cddfe1d
Rey <reycrisbf@gmail.com>
2024-04-08 13:36:28 -0400

    Preparando los modelos

--- app/Models/CentroMedico.php
+++ app/Models/CentroMedico.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class CentroMedico extends Model
 {
     use HasFactory;
+
+    protected $table = 'centros_medicos';
+
+    public function doctores(): HasMany {
+        return $this->hasMany(Doctor::class);
+    }
+
+    public function recetas(): HasMany {
+        return $this->hasMany(Receta::class);
+    }
+
+    protected $fillable = [
+        'nombre',
+        'celular',
+        'direccion',
+        'correo',
+    ];
 }
